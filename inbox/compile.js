98de9f88523393e23c272d804bca5c750ce1b7bc
Zubair Quraishi <fquraish@redhat.com>
2021-11-23 12:50:12 +0100

    added

---
+++ inbox/compile.js
@@ -0,0 +1,15 @@
+// compile code will go here
+const path = require('path')
+const fs = require('fs')
+const solc = require('solc')
+
+const inboxPath = path.resolve(
+                    __dirname,
+                    "contracts",
+                    "inbox.sol")
+
+const source = fs.readFileSync(inboxPath, "utf8")
+
+let out = solc.compile(source,1)
+
+module.exports = out.contracts[':Inbox']
