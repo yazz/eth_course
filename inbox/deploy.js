331276a55d31f9fd95e1f16a2ab84b57aab31669
Zubair Quraishi <zubairq@gmail.com>
2021-11-25 17:39:11 +0000

    updating to deploy on metamask

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,11 @@
 // deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const Web3 = require("web3")
+const { interface, bytecode } = require( "./compile" )
+
+const provider = new HDWalletProvider(
+  "science venue decorate hamster brush expire like session energy level hobby laundry",
+  "https://rinkeby.infura.io/v3/52e65f2369ea4194a3883df1d42383c5"
+)
+
+const web3 = new Web3(provider)
